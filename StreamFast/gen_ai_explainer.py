1f2aa8d77cc8116e9082319b89226f05c01e6a2a
aishuse <aiswarya180@gmail.com>
2025-09-14 03:40:50 -0400

    added llm explanation

--- StreamFast/gen_ai_explainer.py
+++ StreamFast/gen_ai_explainer.py
@@ -1,14 +1,14 @@
 from langchain_groq import ChatGroq
 from langchain.schema import HumanMessage
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
-# load_dotenv()
+load_dotenv()
 api_key = os.environ.get("GROQ_API_KEY")
 
 class GenerativeAIExplainer:
     def __init__(self):
         # Initialize the Groq chat model
-        self.client = ChatGroq(model="llama3-7b", api_key=api_key)
+        self.client = ChatGroq(model="llama-3.1-8b-instant", api_key=api_key)
 
     def explain_prediction(self, customer_features: dict, prediction: int) -> str:
         prompt = f"""
